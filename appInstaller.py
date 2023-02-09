694f91c5fb96550b41d2e73dc660b002bb4457b5
Bela Schaum <schaumb@users.noreply.github.com>
2023-02-09 11:35:57 +0100

    Update appInstaller.py

--- appInstaller.py
+++ appInstaller.py
@@ -2,31 +2,30 @@ import streamlit as st
 import os
 import requests as rq
 import json
-import base64
-import time
 from github import Github
 
 
-############### CONFIGURATION ################
+# ############## CONFIGURATION ############### #
 FLOWIDEAPPS_ORG = 'FloWide-Apps'
 keycloak = 'http://keycloak'
 scriptHandler = 'http://script_handler'
 server = os.environ['SERVER']
-##############################################
+# ############################################ #
 
 
 # GET request with token and error handling
 def rqgetAuth(url, token):
-    rv = rq.get(url, headers = {'Authorization' : 'Bearer ' + token})
+    rv = rq.get(url, headers={'Authorization': 'Bearer ' + token})
     if rv.status_code != 200:
         st.error('HTTP request failed!')
         st.write('*Additional information for the error:* ', rv.json())
         st.stop()
     return rv.json()
 
+
 # POST request with token and error handling
 def rqpostAuth(url, token, data):
-    rv = rq.post(url, headers = {'Authorization' : 'Bearer ' + token}, data = data)
+    rv = rq.post(url, headers={'Authorization': 'Bearer ' + token}, data=data)
     if rv.status_code != 200:
         st.error('HTTP request failed!')
         st.write('*Additional information for the error:* ', rv.json())
@@ -42,7 +41,8 @@ st.header('Collecting data')
 # get user info from keycloak
 st.write('Get user info...')
 token = st.experimental_get_query_params()['token'][0]   # get keycloak token
-userinfo_endpoint = (rq.get(f'{keycloak}/auth/realms/{server}-gw/.well-known/openid-configuration').json())['userinfo_endpoint']
+usr_ep = f'{keycloak}/auth/realms/{server}-gw/.well-known/openid-configuration'
+userinfo_endpoint = (rq.get(usr_ep).json())['userinfo_endpoint']
 userinfo = rqgetAuth(userinfo_endpoint, token)
 if 'github_token' in userinfo and 'token' in userinfo['github_token']:
     githubToken = userinfo['github_token']['token']
@@ -81,7 +81,8 @@ for repo in flowideAppsReposList:
             st.write('**INSTALLED** as ', installedAs)
         # offer to install
         else:
-            # the installed name is not necessary to be the same as the github repo name
+            # the installed name is not necessary to be the same
+            # as the github repo name
             installAs = st.text_input('Install as', repo.name, key=-repo.id)
             # check name (already exists, invalid)
             nameOk = True
@@ -96,9 +97,9 @@ for repo in flowideAppsReposList:
             if nameOk and st.button('INSTALL', key=repo.id):
                 st.write(f'Installing *{repo.name}* from github...')
                 with st.spinner('in progress'):
-                    postData = {'name' : installAs, 'from_url' : repo.clone_url}
+                    postData = {'name': installAs, 'from_url': repo.clone_url}
                     if githubToken:
-                        postData.update({'oauth_token' : githubToken})
-                    ret = rqpostAuth(f'{scriptHandler}/repo/import', token, json.dumps(postData))
+                        postData.update({'oauth_token': githubToken})
+                    ret = rqpostAuth(f'{scriptHandler}/repo/import', token,
+                                     json.dumps(postData))
                 st.write('Done!')
-                
