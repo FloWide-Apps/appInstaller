dc27428f08ae4d7cf5716ee9f0de9f6aac2eb6a5
Gabor Szabo <gabor.szabo@omt.hu>
2023-02-10 17:57:05 +0100

    for new ScriptHandler version

--- appInstaller.py
+++ appInstaller.py
@@ -5,18 +5,18 @@ import json
 from github import Github
 
 
-# ############## CONFIGURATION ############### #
+# ############## CONFIGURATION ################
 FLOWIDEAPPS_ORG = 'FloWide-Apps'
-keycloak = 'http://keycloak'
-scriptHandler = 'http://script_handler'
+# KEYCLOAK_ADDRESS = 'http://keycloak'
+SCRIPTHANDLER_ADDRESS = 'http://script_handler'
 server = os.environ['SERVER']
-# ############################################ #
+# #############################################
 
 
 # GET request with token and error handling
 def rqgetAuth(url, token):
     rv = rq.get(url, headers={'Authorization': 'Bearer ' + token})
-    if rv.status_code != 200:
+    if rv.status_code < 200 or rv.status_code >= 300:
         st.error('HTTP request failed!')
         st.write('*Additional information for the error:* ', rv.json())
         st.stop()
@@ -25,8 +25,8 @@ def rqgetAuth(url, token):
 
 # POST request with token and error handling
 def rqpostAuth(url, token, data):
-    rv = rq.post(url, headers={'Authorization': 'Bearer ' + token}, data=data)
-    if rv.status_code != 200:
+    rv = rq.post(url, headers={'Authorization': 'Bearer ' + token}, data = data)
+    if rv.status_code < 200 or rv.status_code >= 300:
         st.error('HTTP request failed!')
         st.write('*Additional information for the error:* ', rv.json())
         st.stop()
@@ -36,31 +36,33 @@ def rqpostAuth(url, token, data):
 st.title('FloWide App Installer')
 st.write(f'Current server: **{server}**')
 
-st.header('Collecting data')
-
-# get user info from keycloak
-st.write('Get user info...')
 token = st.experimental_get_query_params()['token'][0]   # get keycloak token
-usr_ep = f'{keycloak}/auth/realms/{server}-gw/.well-known/openid-configuration'
-userinfo_endpoint = (rq.get(usr_ep).json())['userinfo_endpoint']
-userinfo = rqgetAuth(userinfo_endpoint, token)
-if 'github_token' in userinfo and 'token' in userinfo['github_token']:
-    githubToken = userinfo['github_token']['token']
-else:
+# userinfo_endpoint = (rq.get(f'{KEYCLOAK_ADDRESS}/auth/realms/{server}-gw/.well-known/openid-configuration').json())['userinfo_endpoint']
+# userinfo = rqgetAuth(userinfo_endpoint, token)
+# if 'github_token' in userinfo and 'token' in userinfo['github_token']:
+#     githubToken = userinfo['github_token']['token']
+# else:
+#     githubToken = None
+
+githubToken = st.text_input('GitHub token to access also private apps')
+if githubToken == '':
     githubToken = None
 
-st.write('Get available apps from github...')
-# initialize github object
-g = Github(githubToken)
-# get repos from organization
-org = g.get_organization(FLOWIDEAPPS_ORG)
-flowideAppsReposList = org.get_repos()
+with st.spinner('Collecting data...'):
 
-# get current repos from workbench
-st.write('Get list of apps currently in workbench...')
-localReposList = rqgetAuth(f'{scriptHandler}/repo', token)
+    # initialize github object
+    g = Github(githubToken)
+    # get repos from organization
+    org = g.get_organization(FLOWIDEAPPS_ORG)
+    flowideAppsReposList = org.get_repos()
+    # get tags for repos
+    flowideAppsReposTags = {}
+    for repo in flowideAppsReposList:
+        repoTags = repo.get_tags()
+        flowideAppsReposTags[repo.name] = repoTags
 
-st.header('App list')
+    # get current repos from workbench
+    localReposList = rqgetAuth(f'{SCRIPTHANDLER_ADDRESS}/repo', token)
 
 # check for already installed apps
 for repo in flowideAppsReposList:
@@ -70,9 +72,7 @@ for repo in flowideAppsReposList:
         # check whether it is already installed
         installedAs = None
         for lr in localReposList.values():
-            lrif = ''
-            if lr['apps_config']['metadata']:
-                lrif = lr['apps_config']['metadata'].get('imported_from', '')
+            lrif = lr.get('imported_from', '')
             if lrif == repo.clone_url:
                 installedAs = lr['name']
                 break
@@ -81,9 +81,17 @@ for repo in flowideAppsReposList:
             st.write('**INSTALLED** as ', installedAs)
         # offer to install
         else:
-            # the installed name is not necessary to be the same
-            # as the github repo name
-            installAs = st.text_input('Install as', repo.name, key=-repo.id)
+            # release selection
+            tags = []
+            for tag in flowideAppsReposTags[repo.name]:   # extract tag names
+                tags.append(tag.name)
+            if tags:   # has tags
+                stn = st.selectbox('Select release to install', tags, key='select1-'+str(repo.id))
+                refSelect = 'refs/tags/'+stn   # save reference
+            else:   # no tags
+                refSelect = None
+            # the installed name is not necessary to be the same as the github repo name
+            installAs = st.text_input('Install as', repo.name, key='input1-'+str(repo.id))
             # check name (already exists, invalid)
             nameOk = True
             for lr in localReposList.values():
@@ -94,12 +102,16 @@ for repo in flowideAppsReposList:
             if installAs == '':
                 nameOk = False
             # install!
-            if nameOk and st.button('INSTALL', key=repo.id):
+            if nameOk and st.button('INSTALL', 'button1-'+str(repo.id)):
                 st.write(f'Installing *{repo.name}* from github...')
                 with st.spinner('in progress'):
-                    postData = {'name': installAs, 'from_url': repo.clone_url}
+                    postData = {
+                        'name': installAs,
+                        'from_url': repo.clone_url
+                    }
+                    if refSelect:
+                        postData.update({'ref': refSelect})
                     if githubToken:
                         postData.update({'oauth_token': githubToken})
-                    ret = rqpostAuth(f'{scriptHandler}/repo/import', token,
-                                     json.dumps(postData))
+                    ret = rqpostAuth(f'{SCRIPTHANDLER_ADDRESS}/repo/import', token, json.dumps(postData))
                 st.write('Done!')
